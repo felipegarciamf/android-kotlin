99333aeab508568a0e16f086747daa7b08f2945b
felipe <silas.garcia@mfconsulting.com.br>
2020-06-02 21:53:28 -0300

    subindo aplicação kotln

---
+++ FinancasK/app/src/main/java/com/alura/financask/ui/activity/ListaTransacoesAcitivity.kt
@@ -0,0 +1,14 @@
+package com.alura.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaTransacoesAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
+
