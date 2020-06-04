891814cd91e932a6877a3a31d41ff54591b99761
felipe <silas.garcia@mfconsulting.com.br>
2020-06-03 23:29:10 -0300

    arrumando configuração para cardview para android x

--- FinancasK/app/src/main/java/com/alura/financask/ui/activity/ListaTransacoesAcitivity.kt
+++ FinancasK/app/src/main/java/com/alura/financask/ui/activity/ListaTransacoesAcitivity.kt
@@ -1,13 +1,22 @@
 package com.alura.financask.ui.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.alura.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesAcitivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
 
+        val transacoes: List<String> = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter<Any>(this, android.R.layout.simple_list_item_1)
+
+        lista_transacoes_listview.adapter = arrayAdapter
     }
 
 }
